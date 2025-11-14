12ff154bc0d9ccb3b25c56085e66fb272cfb629a
hicha <hichamakil2018@gmail.com>
2025-11-14 17:30:49 +0100

    Finish  User model with Full CRUD operations

--- Mian.py
+++ Mian.py
@@ -1,4 +1,5 @@
-from db_config import get_connection
+from dbs.db_config import get_connection
+from Models.User_model import *
 
 
 try :
@@ -10,3 +11,26 @@ except Exception as e:
 finally:
     if conn in locals() and conn:
         conn.close()
+
+
+
+print("Adding user...")
+add_user("Hicham", "Akil", "AA123", "hicham@mail.com")
+
+print("ALL USERS:")
+print(get_all_users())
+
+print("GET USER ID 1:")
+print(get_user_bid(1))
+
+print("UPDATING...")
+update_user_info(1, new_email="new@test.com")
+
+print("AFTER UPDATE:")
+print(get_user_bid(1))
+
+print("DELETING...")
+delete_user(1)
+
+print("USERS AFTER DELETE:")
+print(get_all_users())
--- Models/User_model.py
+++ Models/User_model.py
@@ -9,7 +9,9 @@ def add_user(name,prenom,cin,email):
 """,(name,prenom,cin,email))
     conn.commit()
     cursor.close()
-    conn.close
+    conn.close()
+
+
 def get_all_users():
     conn=get_connection()
     cursor=conn.cursor()
@@ -18,3 +20,58 @@ def get_all_users():
     cursor.close()
     conn.close()
     return users
+
+
+def get_user_bid(id):
+    conn=get_connection()
+    cursor=conn.cursor()
+    cursor.execute("select * from users where user_id=:1",(id,))
+    user=cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return user
+
+
+def update_user_info(user_id, new_name=None, new_prenom=None, new_cin=None, new_email=None):
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    fields = []
+    values = {}
+
+    if new_name:
+        fields.append("name = :name")
+        values["name"] = new_name
+
+    if new_prenom:
+        fields.append("prenom = :prenom")
+        values["prenom"] = new_prenom
+
+    if new_cin:
+        fields.append("cin = :cin")
+        values["cin"] = new_cin
+
+    if new_email:
+        fields.append("email = :email")
+        values["email"] = new_email
+
+    if not fields:
+        return False 
+
+    query = f"UPDATE users SET {', '.join(fields)} WHERE user_id = :user_id"
+    values["user_id"] = user_id
+
+    cursor.execute(query, values)
+    conn.commit()
+
+    cursor.close()
+    conn.close()
+    return True
+
+def delete_user(user_id):
+    conn=get_connection()
+    cursor=conn.cursor()
+    cursor.execute("delete from users where user_id=:1",(user_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
